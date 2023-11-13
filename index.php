96d1c4bd6e35b261e913ab04ea8b431edcc66369
Yakiv <example@gmail.com>
2023-11-13 21:33:11 +0200

    finish reviews category

--- index.php
+++ index.php
@@ -696,7 +696,25 @@
       <h2>Клиенты о нас</h2>
       <div class="row">
         <div class="slider_ex">
-          <div class="review-block">
+
+          <!-- <div class="review-block">
+            <div class="media review-avatar">
+              <div class="media-left">
+                <img class="media-object" src="<?php echo bloginfo('template_url'); ?>/assets/img/review/ava.png"
+                  alt="Анна Маслова">
+              </div>
+              <div class="media-body">
+                <p class="review-avatar-name">Анна Маслова, Москва</p>
+              </div>
+            </div>
+            <div class="review-block-text">
+              <h3>Вопрос по недвижимости</h3>
+              <p>Если бы не компания «Найдем Адвоката», то моя семья могла бы совершить роковую ошибку и попасть в лапы
+                к аферистам. Юрист за 2 дня смог досудебно полностью урегулировать конфликт. Спасибо Вам</p>
+            </div>
+          </div> -->
+
+          <!-- <div class="review-block">
             <div class="media review-avatar">
               <div class="media-left">
                 <img class="media-object" src="<?php echo bloginfo('template_url'); ?>/assets/img/review/ava.png"
@@ -759,23 +777,64 @@
               <p>Если бы не компания «Найдем Адвоката», то моя семья могла бы совершить роковую ошибку и попасть в лапы
                 к аферистам. Юрист за 2 дня смог досудебно полностью урегулировать конфликт. Спасибо Вам</p>
             </div>
-          </div>
-          <div class="review-block">
-            <div class="media review-avatar">
-              <div class="media-left">
-                <img class="media-object" src="<?php echo bloginfo('template_url'); ?>/assets/img/review/ava.png"
-                  alt="Анна Маслова">
+          </div> -->
+
+
+
+          <?php
+
+          // выбираем записи, которые нас интересуют
+          $args = array(
+            'category_name' => 'reviews',
+            // Эта строчка була изменена с 'category'    => 0,
+            'numberposts' => 0,
+            // строчка, необходимая, чтобы выводить неограниченное кол-во записей
+            'order' => 'ASC',
+            'post_type' => 'post',
+            'suppress_filters' => true,
+            // suppression of filters of SQL query change
+          );
+
+          // помещаем записи в одну переменную $posts
+          $posts = get_posts($args);
+
+          // запускаем цикл и в нём перебираем все посты по нашим параметрам
+          foreach ($posts as $post) {
+            setup_postdata($post);
+            ?>
+
+            <!-- html-разметка, в которую мы вставляем информацию о записях -->
+
+            <div class="review-block">
+              <div class="media review-avatar">
+                <div class="media-left">
+                  <img class="media-object" src="<?php the_field('image'); ?>" alt="<?php the_title() ?>">
+                </div>
+                <div class="media-body">
+                  <p class="review-avatar-name">
+                    <?php the_title() ?>
+                  </p>
+                </div>
               </div>
-              <div class="media-body">
-                <p class="review-avatar-name">Анна Маслова, Москва</p>
+              <div class="review-block-text">
+                <h3>
+                  <?php the_field('topic') ?>
+                </h3>
+                <p>
+                  <?php the_field('text'); ?>
+                </p>
               </div>
             </div>
-            <div class="review-block-text">
-              <h3>Вопрос по недвижимости</h3>
-              <p>Если бы не компания «Найдем Адвоката», то моя семья могла бы совершить роковую ошибку и попасть в лапы
-                к аферистам. Юрист за 2 дня смог досудебно полностью урегулировать конфликт. Спасибо Вам</p>
-            </div>
-          </div>
+
+            <?php
+          }
+
+          wp_reset_postdata();
+
+          ?>
+
+
+
         </div>
       </div>
     </div>
@@ -883,7 +942,7 @@
     });
   </script>
   <!-- Формы -->
-  <script>
+  <!-- <script>
     $(function () {
       $('form').submit(function (e) {
         var $form = $(this);
@@ -904,7 +963,7 @@
         e.preventDefault();
       });
     });
-  </script>
+  </script> -->
   <!-- Маска -->
   <!-- <script src="js/jquery.maskedinput.min.js"></script> -->
   <script>
